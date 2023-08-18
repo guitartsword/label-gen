3604bbaf99fa5dcd53d65254b3c82c1a182b95c9
Isaias Valle <guitartsword@gmail.com>
2023-08-17 22:39:49 -0600

    Refactor create_image Load products from csv Improve Readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# Mini Label Generator
+Generates image for Phomemo Q30 printer. Default size is 14x40mm
+
+## Setup
+* Install poetry `pip install poetry`
+* Install dependencies `poetry install`
+
+
+## Usage
+* Copy the template.csv to input.csv
+* Update input.csv with your data (text1: top line, text2: bottom line)
+* Shell into env with `poetry shell`
+* Run `python .`
+* Check for `output` folder... Magic!
+
+
+## Example output:
+<img alt="Label Example 1" src="assets/00154 Selfie Negro.png" width="300">
+<img alt="Label Example 2" src="assets/09213 Test Product.png" width="300">
+<img alt="Label Example 3" src="assets/99999 Test Product.png" width="300">
--- app/create_label.py
+++ app/create_label.py
@@ -10,27 +10,22 @@ def create_image(text1, text2, name=None, output_path='output/'):
     img_width = int(width * ppm)
     img_height = int(height * ppm)
 
-    # Create a blank image
+    # Create a blank image with desired properties
     image = Image.new("RGB", (img_width, img_height), "white")
     draw = ImageDraw.Draw(image)
     draw.fontmode = "1"
 
     # Set font properties
-    font_size1 = 4
-    font_size2 = 7
+    font_size1 = 4  # This is the relative size
+    font_size2 = 7  # This is the relative size
     font1 = ImageFont.truetype("fonts/roboto/Roboto-Medium.ttf", round(font_size1*ppm))  # You can change the font and size
     font2 = ImageFont.truetype("fonts/roboto/Roboto-Medium.ttf", round(font_size2*ppm))  # You can change the font and size
 
-    text2_width, text2_height = draw.textsize(text2, font=font2)
-    text2_x = img_width - text2_width
-    text2_y = font_size1*ppm + 0*ppm  # Position the second text right below the first text
+    text2_y = font_size1*ppm  # Position the second text right below the first text
 
     # Draw the texts on the image
-    draw.text((0, 0), text1, font=font1, fill="black")
-    draw.text((0, text2_y), text2, font=font2, fill="black", align="center")
-
-    # Resize the image to the desired dimensions
-    # image = image.resize((int(width * 10), int(height * 10)))  # Multiply by 10 to convert from cm to mm
+    draw.text((0, 0), text1, font=font1, fill="black")  # Text 1
+    draw.text((0, text2_y), text2, font=font2, fill="black", align="center")  # Text 2
 
     # Save the image as PNG
     create_folder(output_path)
